55c207ee60fdd6bc4c97efa07ee62ace62d58da6
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-11-24 14:08:31 +0530

    alert done
    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
+import React,{useState} from 'react';
+import { BsFillSunFill } from 'react-icons/bs';
+import Alert from './components/Alert'
+import List from './components/List'
 
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+
+import {v4 as uuid4} from 'uuid';
+
+const App=()=>{
+
+  const [name,setName] = useState('');
+  const[list,setList]=useState([]);
+  const[isEditing,setIsEditing]=useState(false);
+  const [editId,setEditID]=useState(null);
+
+  const [alert,setAlert]=useState({
+    show:false,
+    message:'',
+    type:'',
+  });
+
+  const showAlert =(show,message,type)=>{
+    setAlert({
+      show,
+      message,
+      type,
+    })
+  };
+  
+  const removeAlert=()=>{
+    setAlert({...alert,show:false});
+  } 
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    if(!name){
+      //display Alert
+      showAlert(true,'Task cannot be empty!','danger')
+    }
+    else if(name &&  isEditing){
+      //deal with edit
+      //edit Alert
+      showAlert(true,'Task Updated','info')
+    }
+    else{
+      //add Alert
+      showAlert(true,'Task added','success')
+    }
+  };
+
+  return(
+    <section>
+      <div className="container">
+        <div className="header">
+          <h1>TODO</h1>
+          <button><BsFillSunFill/></button>
+        </div>
+        <form onSubmit={submitHandler}>
+          {alert.show && <Alert {...alert} removeAlert={removeAlert}/>}
+          <input  
+            type="text" 
+            className="" 
+            placeholder="10 minutes meditation"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+          />
+          <button type="submit">ADD</button>
+        </form>  
+        <div className="list-container">
+          <List items={list}/>
+          <button onClick={()=>setList([])}>Clear All</button>
+        </div>
+      </div> 
+    </section>
+  )
 }
 
 export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react';
+
+const Alert=({message,type,removeAlert})=>{
+useEffect(()=>{
+  const timeout=setTimeout(()=>
+  {
+    removeAlert();
+  },3000)
+  return ()=>clearTimeout(timeout);
+},[]);
+  return(
+    <p className={`alert alert-${type}`}>
+      {message}    
+    </p>
+  )
+}
+
+export default Alert;
